49912e8105798f4b6b40a1560dfca49cc520787e
Adarsh Kumar <adarsh.mca23@gmail.com>
2025-08-07 23:50:13 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/LandingPage'; // Adjust path as needed
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          {/* Add more routes here as needed */}
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          {/* <Route path="/about" element={<About />} /> */}
+          {/* Add more routes here */}
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
